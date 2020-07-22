1813c023f8d685b907f6590c5fecbfedb49af978
EDUARDO ROCHA <ehduardorocha@gmail.com>
2020-07-22 17:21:51 -0300

    Created Marketing mailer

---
+++ app/mailers/marketing_mailer.rb
@@ -0,0 +1,22 @@
+class MarketingMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.marketing_mailer.campaign.subject
+  #
+=begin
+  def campaign
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+=end
+
+
+  def campaign(client, title, body)
+    @body = body
+    mail to: client.email, subject: title
+  end
+
+end
